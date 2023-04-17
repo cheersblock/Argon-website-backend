1fd5f3ff8c320fa1f40ec0fe03b029d4aabb6ff6
cheersblock <112495081+cheersblock@users.noreply.github.com>
2023-04-17 15:08:37 +0500

    Update caseStudies.controller.js

--- src/controllers/caseStudies.controller.js
+++ src/controllers/caseStudies.controller.js
@@ -57,3 +57,46 @@ exports.getCaseStudy = async (req, res) => {
   }
 };
 
+exports.updateCaseStudy = async (req, res) => {
+  const { title } = req.body;
+  console.log(req.body);
+  try {
+    await CaseStudies.update(
+      {
+        title
+      },
+      { where: { id: req.params.id } }
+    );
+
+    const caseStudy = await CaseStudies.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).send({
+      status: "success",
+      data: caseStudy,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+exports.deleteCaseStudy = async (req, res) => {
+  try {
+    if (req.params.id != 1) {
+      await CaseStudies.destroy({ where: { id: req.params.id } });
+
+      res.status(200).send({
+        status: "Deleted Successfully",
+      });
+    } else {
+      res.status(400).send({
+        status: "Bad Request",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
