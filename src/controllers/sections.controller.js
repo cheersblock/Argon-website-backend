4d072c5c93855cdd3d764fe17a326f03ac10ca9f
cheersblock <112495081+cheersblock@users.noreply.github.com>
2023-04-17 15:09:04 +0500

    Update sections.controller.js

--- src/controllers/sections.controller.js
+++ src/controllers/sections.controller.js
@@ -82,3 +82,49 @@ exports.getSections = async (req, res) => {
   }
 };
 
+exports.updateSection = async (req, res) => {
+    const {  title, description, image, caseStudyId  } =
+      req.body;
+    console.log(req.body);
+    try {
+      await Sections.update(
+        {
+            title, description, image, caseStudyId 
+        },
+        { where: { id: req.params.id } }
+      );
+  
+      const section = await Sections.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      res.status(200).send({
+        status: "success",
+        data: section,
+      });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  };
+
+  exports.deleteSection = async (req, res) => {
+    try {
+      if (req.params.id != 1) {
+        await Sections.destroy(
+          { where: { id: req.params.id } }
+        );
+  
+        res.status(200).send({
+          status: "Deleted Successfully"
+        });
+      } else {
+        res.status(400).send({
+          status: "Bad Request"
+        });
+      }
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  };
